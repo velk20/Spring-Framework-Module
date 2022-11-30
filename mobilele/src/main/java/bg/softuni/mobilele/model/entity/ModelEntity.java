8e90f0885a4d95ddc3637af29a4744e09718b588
Angel <48069264+velk20@users.noreply.github.com>
2022-11-30 15:25:22 +0200

    Init MOBILELELE

---
+++ mobilele/src/main/java/bg/softuni/mobilele/model/entity/ModelEntity.java
@@ -0,0 +1,19 @@
+package bg.softuni.mobilele.model.entity;
+
+import bg.softuni.mobilele.model.entity.enums.CategoryEnum;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "models")
+public class ModelEntity extends BaseEntity {
+    private String name;
+    @Enumerated(EnumType.STRING)
+    private CategoryEnum categoryEnum;
+    private String imageUrl;
+    private int startYear;
+    private Long endYear;
+}
