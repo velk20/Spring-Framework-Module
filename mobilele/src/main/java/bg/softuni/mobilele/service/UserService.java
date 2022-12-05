caa674a86245d4102fb48cf95ccff354ea6c42e7
Angel <48069264+velk20@users.noreply.github.com>
2022-12-05 17:07:04 +0200

    Login, register and logout at Mobilelele

---
+++ mobilele/src/main/java/bg/softuni/mobilele/service/UserService.java
@@ -0,0 +1,70 @@
+package bg.softuni.mobilele.service;
+
+import bg.softuni.mobilele.model.dto.UserLoginDTO;
+import bg.softuni.mobilele.model.dto.UserRegisterDTO;
+import bg.softuni.mobilele.model.entity.UserEntity;
+import bg.softuni.mobilele.repository.UserRepository;
+import bg.softuni.mobilele.user.CurrentUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
+    private final UserRepository userRepository;
+    private final CurrentUser currentUser;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, CurrentUser currentUser, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.currentUser = currentUser;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void registerAndLogin(UserRegisterDTO userRegisterDTO) {
+        UserEntity newUser = new UserEntity()
+                .setActive(true)
+                .setEmail(userRegisterDTO.getEmail())
+                .setFirstName(userRegisterDTO.getFirstName())
+                .setLastName(userRegisterDTO.getLastName())
+                .setPassword(passwordEncoder.encode(userRegisterDTO.getPassword()));
+
+        UserEntity toLoggedUser = userRepository.save(newUser);
+        this.login(toLoggedUser);
+    }
+
+    public boolean login(UserLoginDTO loginDTO) {
+        Optional<UserEntity> userOpt = userRepository.findByEmail(loginDTO.getUsername());
+
+        if (userOpt.isEmpty()) {
+            LOGGER.warn("User with name [{}] not found.", loginDTO.getUsername());
+            return false;
+        }
+
+        var rawPassword = loginDTO.getPassword();
+        var hashedPassword = userOpt.get().getPassword();
+
+        boolean success = passwordEncoder.matches(rawPassword, hashedPassword);
+        if (success) {
+            login(userOpt.get());
+        }else{
+            logout();
+        }
+
+        return success;
+    }
+
+    private void login(UserEntity userEntity) {
+        currentUser
+                .setLoggedIn(true)
+                .setName(userEntity.getFirstName() + " " + userEntity.getLastName());
+    }
+
+    public void logout() {
+        currentUser.clear();
+    }
+}
---
+++ mobilele/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users(id,email,first_name,Last_name,image_url,is_active,password)
+VALUES (1,'angel@admin.com','Angel','Mladenov',null,1,'strong_pass'),
+       (2,'admin','admin','admin',null,1,'admin');
