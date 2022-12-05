34402ace91e40bad1d45119954a87489e2cdee53
Angel <48069264+velk20@users.noreply.github.com>
2022-12-05 18:04:27 +0200

    Finished mobilele part 1

--- mobilele/src/main/resources/data.sql
+++ mobilele/src/main/resources/data.sql
@@ -1,3 +1,4 @@
-insert into users(id,email,first_name,Last_name,image_url,is_active,password)
-VALUES (1,'angel@admin.com','Angel','Mladenov',null,1,'strong_pass'),
-       (2,'admin','admin','admin',null,1,'admin');
+# insert into users(id,email,first_name,Last_name,image_url,is_active,password)
+# VALUES (1,'angel@admin.com','Angel','Mladenov',null,1,'strong_pass'),
+#        (2,'admin','admin','admin',null,1,'admin'),
+#        (3,'admin','admin','admin',null,1,'admin');
